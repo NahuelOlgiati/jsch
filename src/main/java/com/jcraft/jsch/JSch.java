00f5ae5a987fe8235002d21a64f7e4db2ec9acf4
nolgiati <nolgiati@nolgiati-pc>
2023-11-17 14:20:44 -0300

    java 6 compatibility

--- src/main/java/com/jcraft/jsch/JSch.java
+++ src/main/java/com/jcraft/jsch/JSch.java
@@ -193,7 +193,6 @@ public class JSch {
     config.put("pbkdf2-hmac-sha384", "com.jcraft.jsch.jce.PBKDF2HMACSHA384");
     config.put("pbkdf2-hmac-sha512", "com.jcraft.jsch.jce.PBKDF2HMACSHA512");
     config.put("bcrypt", "com.jcraft.jsch.jbcrypt.JBCrypt");
-    config.put("argon2", "com.jcraft.jsch.bc.Argon2");
     config.put("scrypt", "com.jcraft.jsch.bc.SCrypt");
 
     if (JavaVersion.getVersion() >= 11) {
